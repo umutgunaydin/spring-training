c657c4155d9698aaf5c7747ae71b83566cf8333e
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-02-24 14:46:52 +0300

    spring-07 main method is created

--- spring-07-SpringBoot-Demo/src/main/java/com/company/CompanyApplication.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/company/CompanyApplication.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.company.model.Comment;
+import com.company.service.CommentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,7 +11,14 @@ public class CompanyApplication {
 
     public static void main(String[] args) {
 
+        Comment comment=new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
         ApplicationContext container = SpringApplication.run(CompanyApplication.class, args);
+
+        CommentService commentService=container.getBean(CommentService.class);
+        commentService.publishComment(comment);
 
     }
 
