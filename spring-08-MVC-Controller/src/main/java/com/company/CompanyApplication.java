a02f4b662c15dca8a216154f71313911c6c55ca9
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-03-05 18:25:27 +0300

    controller package and home.html are created

--- spring-08-MVC-Controller/src/main/java/com/company/CompanyApplication.java
+++ spring-08-MVC-Controller/src/main/java/com/company/CompanyApplication.java
@@ -2,8 +2,9 @@ package com.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 @SpringBootApplication
+//@SpringBootApplication(exclude = {AopAutoConfiguration.class})
 public class CompanyApplication {
 
 	public static void main(String[] args) {
