e45b754957a9a185cb15f487037ccc0f2ec12bf1
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-03-06 14:59:25 +0300

    @PathVariable is added to CarController

--- spring-09-MVC-Model/src/main/java/com/company/controller/CarController.java
+++ spring-09-MVC-Model/src/main/java/com/company/controller/CarController.java
@@ -2,6 +2,7 @@ package com.company.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -25,7 +26,15 @@ public class CarController {
 
     //localhost:8080/info3?make=Honda&year=2015
     @RequestMapping("/info3")
-    public String carInfo3(@RequestParam String make,@RequestParam int year, Model model){
+    public String carInfo3(@RequestParam String make,@RequestParam int year, Model model){//this is optional
+
+        model.addAttribute("make",make);
+        model.addAttribute("year",year);
+
+        return "car/car-info";
+    }
+    @RequestMapping("/info/{make}/{year}") //localhost:8080/info/honda/2015 !!!!->this is not optional
+    public String getCarInfo(@PathVariable String make,@PathVariable int year,Model model){
 
         model.addAttribute("make",make);
         model.addAttribute("year",year);
