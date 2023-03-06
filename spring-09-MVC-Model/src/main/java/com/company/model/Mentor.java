c7fd33a4fa5ae25b7d1202e1eb7d702a6c714fab
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-03-06 12:58:56 +0300

    mentor,mentorController classes and credentials.html are created

---
+++ spring-09-MVC-Model/src/main/java/com/company/model/Mentor.java
@@ -0,0 +1,16 @@
+package com.company.model;
+
+import com.company.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Mentor {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Gender gender;
+
+}
