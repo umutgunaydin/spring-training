f5e5ff640ace13eef8a7c225465da1f8db25d8b5
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-03-25 18:27:58 +0300

    StudentController and register.html are updated

--- spring-10-Thymeleaf/src/main/java/com/company/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/company/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.company.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/student")
@@ -17,10 +18,12 @@ public class StudentController {
         return "student/register";
     }
 
+
+
     @RequestMapping("/welcome")
-    public String welcome(){
+    public String welcome(@RequestParam String name){
 
-
+        System.out.println(name);
 
         return "student/welcome";
     }
