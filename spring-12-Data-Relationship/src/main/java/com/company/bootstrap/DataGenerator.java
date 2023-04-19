1ff89f9542873a2feb6caa1d93e4eef45896405c
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-04-19 06:28:29 +0300

    merchant repository created

--- spring-12-Data-Relationship/src/main/java/com/company/bootstrap/DataGenerator.java
+++ spring-12-Data-Relationship/src/main/java/com/company/bootstrap/DataGenerator.java
@@ -1,8 +1,10 @@
 package com.company.bootstrap;
 
+import com.company.entity.Merchant;
 import com.company.entity.Payment;
 import com.company.entity.PaymentDetail;
 import com.company.enums.Status;
+import com.company.repository.MerchantRepository;
 import com.company.repository.PaymentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,9 +16,11 @@ import java.time.LocalDate;
 public class DataGenerator implements CommandLineRunner {
 
     private final PaymentRepository paymentRepository;
+    private final MerchantRepository merchantRepository;
 
-    public DataGenerator(PaymentRepository paymentRepository) {
+    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository) {
         this.paymentRepository = paymentRepository;
+        this.merchantRepository = merchantRepository;
     }
 
     @Override
@@ -32,12 +36,18 @@ public class DataGenerator implements CommandLineRunner {
 
         payment2.setPaymentDetail(paymentDetail2);
 
+        Merchant merchant1 = new Merchant("AmazonSubMerchant","M123",new BigDecimal("0.25"),new BigDecimal("3.25"),5);
+
+        payment1.setMerchant(merchant1);
+        payment2.setMerchant(merchant1);
+
+        merchantRepository.save(merchant1);
+
         paymentRepository.save(payment1);
         paymentRepository.save(payment2);
 
-        System.out.println(paymentRepository.findById(2L).get().getPaymentDetail().getCommissionAmount());
 
-        paymentRepository.delete(payment1);
+
 
 
     }
---
+++ spring-12-Data-Relationship/src/main/java/com/company/repository/MerchantRepository.java
@@ -0,0 +1,9 @@
+package com.company.repository;
+
+import com.company.entity.Merchant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MerchantRepository extends JpaRepository<Merchant,Long> {
+}
