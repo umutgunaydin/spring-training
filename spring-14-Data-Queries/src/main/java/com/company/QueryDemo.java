446fa20dce1cd6b2de0e4f0f542f664eafabd9f1
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-05-03 14:56:06 +0300

    DepartmentRepository updated

--- spring-14-Data-Queries/src/main/java/com/company/QueryDemo.java
+++ spring-14-Data-Queries/src/main/java/com/company/QueryDemo.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.repository.DepartmentRepository;
 import com.company.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,18 +9,29 @@ import org.springframework.stereotype.Component;
 public class QueryDemo implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
 
 
-    public QueryDemo(RegionRepository regionRepository) {
+    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
         this.regionRepository = regionRepository;
+        this.departmentRepository = departmentRepository;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        System.out.println("***************REGIONS**************");
         System.out.println("findByCountry:"+regionRepository.findByCountry("Canada"));
         System.out.println("findByCountryContains:"+regionRepository.findByCountryContains("United"));
         System.out.println("findByCountryContainsOrderByRegionDesc:"+regionRepository.findByCountryContainsOrderByRegionDesc("United"));
         System.out.println("findTop2ByCountry:"+regionRepository.findTop2ByCountry("United States"));
+
+        System.out.println("***************REGIONS**************");
+        System.out.println("findByDepartment:"+departmentRepository.findByDepartment("Furniture"));
+        System.out.println("findByDivision:"+departmentRepository.findByDivision("Health"));
+        System.out.println("findByDivisionEndingWith:"+departmentRepository.findByDivisionEndingWith("ics"));
+        System.out.println("findDistinctTop3ByDivisionIsContaining:"+departmentRepository.findDistinctTop3ByDivisionIsContaining("Hea"));
+
     }
 }
--- spring-14-Data-Queries/src/main/java/com/company/repository/DepartmentRepository.java
+++ spring-14-Data-Queries/src/main/java/com/company/repository/DepartmentRepository.java
@@ -4,8 +4,23 @@ import com.company.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, String> {
 
+    //display all departments in the Furniture Department
+    List<Department> findByDepartment(String department);
+
+    //display all departments in Health Division
+    List<Department> findByDivision(String division);
+    List<Department> findByDivisionIs(String division);  // all are same
+    List<Department> findByDivisionEquals(String division);
+
+    //display all departments with division name ends with 'ics'
+    List<Department> findByDivisionEndingWith(String division);
+
+    //display top 3 departments with division name includes 'Hea' without duplicates
+    List<Department> findDistinctTop3ByDivisionIsContaining(String division);
 
 }
