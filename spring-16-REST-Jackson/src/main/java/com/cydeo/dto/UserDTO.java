8f402c3edfc6e593e3a41edbfe5a0c3fe5026a80
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-05-25 17:06:54 +0300

    Jackson done

---
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/UserDTO.java
@@ -0,0 +1,25 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDTO {
+
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    private String username;
+    private UserRole role;
+
+    @JsonManagedReference  // This field is going to be serialized // won't be sent as response
+    private AccountDTO account;
+
+}
