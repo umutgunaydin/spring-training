c8f6dbf899daab5acc557708834f1cf197e04a8a
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-05-29 14:21:35 +0300

    Spring Security

---
+++ spring-20-REST-Security-Keycloak-LicenseApp/src/main/java/com/company/service/impl/LicenseServiceImpl.java
@@ -0,0 +1,66 @@
+package com.company.service.impl;
+
+import com.company.client.OrganizationFeignClient;
+import com.company.model.License;
+import com.company.model.Organization;
+import com.company.repository.LicenseRepository;
+import com.company.service.LicenseService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LicenseServiceImpl implements LicenseService {
+
+    private final LicenseRepository licenseRepository;
+    private final OrganizationFeignClient feignClient;
+
+    public LicenseServiceImpl(LicenseRepository licenseRepository, OrganizationFeignClient feignClient) {
+        this.licenseRepository = licenseRepository;
+        this.feignClient = feignClient;
+    }
+
+    public License getLicense(Long licenseId, Long organizationId) throws Exception {
+
+        License license = licenseRepository.findById(licenseId).orElseThrow(() -> new Exception("License not found"));
+
+        Organization organization = retrieveOrganizationInfo(organizationId);
+
+        if (organization != null) {
+
+            license.setOrganizationName(organization.getName());
+            license.setContactName(organization.getContactName());
+            license.setContactEmail(organization.getContactEmail());
+            license.setContactPhone(organization.getContactPhone());
+
+        }
+
+        return license;
+
+    }
+
+    private Organization retrieveOrganizationInfo(Long organizationId) {
+        return feignClient.getOrganization(organizationId);
+    }
+
+    public License createLicense(License license) {
+        licenseRepository.save(license);
+        return license;
+    }
+
+    public License updateLicense(License license) {
+        licenseRepository.save(license);
+        return license;
+    }
+
+    public String deleteLicense(Long licenseId) throws Exception {
+        License license = licenseRepository.findById(licenseId).orElseThrow(() -> new Exception("License not found"));
+        licenseRepository.delete(license);
+        return "License deleted";
+    }
+
+    public List<License> getLicensesByOrganization(Long organizationId) {
+        return licenseRepository.findByOrganizationId(organizationId);
+    }
+
+}
