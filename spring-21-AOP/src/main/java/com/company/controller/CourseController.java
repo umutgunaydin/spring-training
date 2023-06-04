5af62e1bd91ec0f97688f451c92f207b9e92f66b
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-06-04 15:31:19 +0300

    spring-21 created

---
+++ spring-21-AOP/src/main/java/com/company/controller/CourseController.java
@@ -0,0 +1,59 @@
+package com.company.controller;
+
+import com.company.annotation.LoggingAnnotation;
+import com.company.dto.CourseDTO;
+import com.company.service.CourseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses/api/v1")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<CourseDTO> getAllCourses() {
+//        throw new RuntimeException("My Exception");
+        List<CourseDTO> list = courseService.getCourses();
+        return list;
+    }
+
+    @LoggingAnnotation
+    @GetMapping("/{id}")
+    public CourseDTO getCourseById(@PathVariable("id") Long courseId) {
+        return courseService.getCourseById(courseId);
+    }
+
+    @GetMapping("/category/{name}")
+    public List<CourseDTO> getCourseByCategory(@PathVariable("name") String category) {
+        return courseService.getCoursesByCategory(category);
+    }
+
+    @LoggingAnnotation
+    @PostMapping
+    public CourseDTO createCourse(@RequestBody CourseDTO course) {
+        return courseService.createCourse(course);
+    }
+
+    @PutMapping("/{id}")
+    public void updateCourse(@PathVariable("id") Long courseId, @RequestBody CourseDTO course) {
+        courseService.updateCourse(courseId, course);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCourseById(@PathVariable("id") Long courseId) {
+        courseService.deleteCourseById(courseId);
+    }
+
+    @DeleteMapping
+    public void deleteCourses() {
+        courseService.deleteCourses();
+    }
+
+}
---
+++ spring-21-AOP/src/main/java/com/company/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package com.company.repository;
+
+import com.company.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findAllByCategory(String category);
+
+}
