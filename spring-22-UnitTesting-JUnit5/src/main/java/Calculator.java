be1221633cc200ae2568b4e6e5e5149159450736
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-06-23 01:01:07 +0300

    before after methods added

--- spring-22-UnitTesting-JUnit5/src/main/java/Calculator.java
+++ spring-22-UnitTesting-JUnit5/src/main/java/Calculator.java
@@ -5,8 +5,11 @@ public class Calculator {
         return num1+num2;
     }
 
-//    public static int add2(int num1,int num2){
-//        return num1+num2;
-//    }
+    public static int add2(int num1,int num2) throws IllegalArgumentException {
+        if (num1>num2){
+            throw new IllegalArgumentException("My exception");
+        }
+        return num1+num2;
+    }
 
 }
