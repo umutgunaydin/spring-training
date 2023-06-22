d33d1456155e33662deb06e9ef14f4ae4a0c7159
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-06-23 00:48:06 +0300

    assert method done

--- spring-22-UnitTesting-JUnit5/src/main/java/Calculator.java
+++ spring-22-UnitTesting-JUnit5/src/main/java/Calculator.java
@@ -1,7 +1,12 @@
 public class Calculator {
 
+    public static String operator="add";
     public static int add(int num1,int num2){
         return num1+num2;
     }
+
+//    public static int add2(int num1,int num2){
+//        return num1+num2;
+//    }
 
 }
